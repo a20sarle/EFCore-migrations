51a340d463bd03111991bde4dd29ae8e623b81f3
Sara Leth <Sara.Leth@volvo.com>
2022-06-22 10:10:20 +0200

    Add project files.

---
+++ EntityFrameworkExample/Models/SqlContext.cs
@@ -0,0 +1,37 @@
+﻿namespace MigrationCodeFirst.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    public class SqlContext : DbContext
+    {
+        public virtual DbSet<SqlWorker>? Workers { get; set; }
+
+        public SqlContext()
+        {
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<SqlWorker>(entity =>
+            {
+                entity.ToTable("workers");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever(); //.HasDefaultValueSql("(newsequentialid())"); ?
+
+                entity.Property(e => e.FirstName)
+                    .HasColumnName("firstname")
+                    .HasMaxLength(255);
+            });
+        }
+    }
+}
---
+++ EntityFrameworkExample/Models/SqlWorker.cs
@@ -0,0 +1,8 @@
+﻿namespace MigrationCodeFirst.Models
+{
+    public class SqlWorker
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+    }
+}
